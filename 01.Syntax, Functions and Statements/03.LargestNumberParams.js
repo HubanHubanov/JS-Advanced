d3d4cc8d2008c6baffa06b22808e613903084e5b
HubanHubanov <huban.hubanov@gmail.com>
2024-01-16 23:32:35 +0200

    add 03.LargestNumberParams

---
+++ 01.Syntax, Functions and Statements/03.LargestNumberParams.js
@@ -0,0 +1,7 @@
+function laregestNumber(...params) {
+  console.log(`The largest number is ${Math.max(...params)}.`);
+}
+
+laregestNumber(5, -3, 16);
+
+laregestNumber(-3, -5, -22.5, 200, 4, 1000, 55, 66);
