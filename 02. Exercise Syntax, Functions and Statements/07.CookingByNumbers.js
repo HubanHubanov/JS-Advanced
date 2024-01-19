946b316f87859bc033ac482ddcf5d6f2a6d01769
HubanHubanov <huban.hubanov@gmail.com>
2024-01-19 09:54:39 +0200

    change 07.CookingByNumbers

--- 02. Exercise Syntax, Functions and Statements/07.CookingByNumbers.js
+++ 02. Exercise Syntax, Functions and Statements/07.CookingByNumbers.js
@@ -4,32 +4,24 @@ function cookingByNumbers(num, ...elements) {
     switch (el) {
       case "chop":
         num /= 2;
-        console.log(num);
         break;
 
       case "dice":
         num = Math.sqrt(num);
-        console.log(num);
         break;
 
       case "spice":
         num += 1;
-        console.log(num);
         break;
 
       case "bake":
         num *= 3;
-        console.log(num);
         break;
 
       case "fillet":
         num = num - 0.2 * num;
-        console.log(num);
         break;
     }
+    console.log(num);
   }
 }
-
-cookingByNumbers("32", "chop", "chop", "chop", "chop", "chop");
-
-cookingByNumbers("9", "dice", "spice", "chop", "bake", "fillet");
