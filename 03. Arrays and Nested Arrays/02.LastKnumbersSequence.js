f393f27c611d1a174b1f8f52f7fea06c644ba880
HubanHubanov <huban.hubanov@gmail.com>
2024-01-20 12:23:15 +0200

    changes 02.LastKnumbers

--- 03. Arrays and Nested Arrays/02.LastKnumbersSequence.js
+++ 03. Arrays and Nested Arrays/02.LastKnumbersSequence.js
@@ -1,23 +1,20 @@
 function solve(sequenseLength, lastNums) {
-    let arr = [1];
-  
-    for (let i = 0; i < sequenseLength - 1; i++) {
-      let sum = 0;
-      for (let j = i; j > i - lastNums; j--) {
-        if (arr[j] !== undefined) {
-          sum += arr[j];
-        }
+  let arr = [1];
+
+  for (let i = 0; i < sequenseLength - 1; i++) {
+    let sum = 0;
+    for (let j = i; j > i - lastNums; j--) {
+      if (arr[j] !== undefined) {
+        sum += arr[j];
       }
-  
-      arr.push(sum);
     }
-  
-    return arr
+
+    arr.push(sum);
   }
-  
-  solve(6, 3);
-  
-  solve(8, 2);
-  
-  
-  
+
+  return arr;
+}
+
+console.log(solve(6, 3));
+
+console.log(solve(8, 2));
