2ffd7de96d34904096f46a9385b062ca3a3b5b0b
HubanHubanov <huban.hubanov@gmail.com>
2024-01-25 17:46:34 +0200

    fix 05.LowestPriceInCities

---
+++ 06.Objects and Composition - Exercise/05.LowestPricesInCities2.js
@@ -0,0 +1,44 @@
+function lowestPricesInCities(input) {
+    let arr = [];
+    let obj = {};
+  
+    for (let el of input) {
+      let [town, product, price] = el.split(" | ");
+      price = Number(price);
+  
+      let found = arr.find((el) => el.product === product);
+  
+      if (!found) {
+        obj = {
+          town,
+          product,
+          price,
+        };
+        arr.push(obj);
+      } else {
+        if (found.price <= price) {
+          continue;
+        } else {
+          found.price = price;
+          found.town = town;
+        }
+      }
+    }
+  
+    for (let el of arr) {
+      console.log(`${el.product} -> ${el.price} (${el.town})`);
+    }
+  }
+
+  lowestPricesInCities([
+    "Sofia City | Audi | 100000",
+    "Sofia City | BMW | 100000",
+    "Sofia City | Mitsubishi | 10000",
+    "Sofia City | Mercedes | 10000",
+    "Sofia City | NoOffenseToCarLovers | 0",
+    "Mexico City | Audi | 1000",
+    "Mexico City | BMW | 99999",
+    "Mexico City | Mitsubishi | 10000",
+    "New York City | Mitsubishi | 1000",
+    "Washington City | Mercedes | 1000",
+  ]);
