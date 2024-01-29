2bc76d72297c066413ccca58c699c7c0539c8027
HubanHubanov <huban.hubanov@gmail.com>
2024-01-30 00:22:14 +0200

    add 03.SumNUmbers

--- 07.DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
+++ 07.DOM Introduction - Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO: sum = num1 + num2
+  let numOne = document.getElementById("num1").value;
+  let numTwo = document.getElementById("num2").value;
+  let sum = document.getElementById("sum");
+
+  sum.value = Number(numOne) + Number(numTwo);
 }
