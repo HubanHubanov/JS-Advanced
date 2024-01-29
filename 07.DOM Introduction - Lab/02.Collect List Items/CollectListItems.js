5b33318a7724302cbfbc76f160ecea77c060d04b
HubanHubanov <huban.hubanov@gmail.com>
2024-01-29 23:03:25 +0200

    add 02.CollectList

--- 07.DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
+++ 07.DOM Introduction - Lab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,13 @@
 function extractText() {
-    // TODO
+    const resultArea = document.getElementById("result")
+   
+    const items = document.getElementById("items")
+
+    const elemnts = Array.from(items.children)
+
+    const resArr = []
+for(let el of elemnts)
+   resArr.push(el.textContent)
+
+   resultArea.textContent = resArr.join("\n")
 }
