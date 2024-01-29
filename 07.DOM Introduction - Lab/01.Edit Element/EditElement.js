60af56151f8796f38fef398fb64859d15d946296
HubanHubanov <huban.hubanov@gmail.com>
2024-01-29 16:20:34 +0200

    add 01.EditElement

--- 07.DOM Introduction - Lab/01.Edit Element/EditElement.js
+++ 07.DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+   let content = element.textContent;
+   element.textContent = content.split(match).join(replacer)
+    
 }
