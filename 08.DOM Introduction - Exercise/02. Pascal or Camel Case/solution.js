e49f82ea758a6281008d6fa931ee6898b697c5bb
HubanHubanov <huban.hubanov@gmail.com>
2024-01-30 22:11:55 +0200

    add 02.PascalOrCamelCase

--- 08.DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
+++ 08.DOM Introduction - Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-  //TODO...
+
+  const text = document.getElementById("text").value;
+
+  const convention = document.getElementById("naming-convention").value;
+
+  let res = "";
+
+  let textArr = text.split(" ");
+
+  if (convention.includes("Camel")) {
+    for (let i = 0; i < textArr.length; i++) {
+      if (i === 0) {
+        res += textArr[i].toLowerCase();
+      } else {
+        res +=
+          textArr[i][0].toUpperCase() + textArr[i].substring(1).toLowerCase();
+      }
+    }
+  } else if (convention.includes("Pascal")) {
+    for (let i = 0; i < textArr.length; i++) {
+      res += textArr[i][0].toUpperCase() + textArr[i].substring(1).toLowerCase();
+    }
+  } else {
+    res = "Error!";
+  }
+
+   document.getElementById("result").textContent = res
 }
