c49efcaaebec70377cc17b99cb8fb962d875f96e
HubanHubanov <huban.hubanov@gmail.com>
2024-01-30 11:11:26 +0200

    add 05.ColorizeTable

--- 07.DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ 07.DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,15 @@
 function colorize() {
-    // TODO
+  const elements = document.getElementsByTagName("tr");
+
+  const arr = Array.from(elements);
+
+  for (let i = 1; i < arr.length; i += 2) {
+    arr[i].style.background = "teal";
+  }
+
+  // const rows = document.querySelectorAll("tr:nth-child(even)");
+
+  // for (let row of rows) {
+  //   row.style.background = "teal";
+  // }
 }
