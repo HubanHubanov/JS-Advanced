e7c856feb5e9e57b1203f880ba4e043517bafcf5
HubanHubanov <huban.hubanov@gmail.com>
2024-01-30 13:51:18 +0200

    add  07.ExtractParenthesis

--- 07.DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 07.DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,31 @@
-function extract(content) {
+function extract(elementId) {
+text = document.getElementById(elementId).textContent
+  let pattern = /\((?<word>[^()]*)\)/g;
 
+  let match = pattern.exec(text);
+
+  let res = [];
+
+  while (match) {
+    res.push(match.groups.word);
+
+    match = pattern.exec(text);
+  }
+
+  let resStr = res.join("; ");
+
+  return resStr;
 }
+
+// function extract(elementId) {
+//     let para = document.getElementById(elementId).textContent;
+//     let pattern = /\(([^)]+)\)/g;
+//     let result = [];
+//     let match = pattern.exec(para);
+//     while(match) {
+//     result.push(match[1]);
+//     match = pattern.exec(para);
+//     }
+//     return result.join('; ');
+//     }
+    
