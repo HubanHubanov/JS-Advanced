c026571dd280b1bcb80ed26d62063ba6a39cfeb7
HubanHubanov <huban.hubanov@gmail.com>
2024-01-31 22:51:59 +0200

    add 03.Accordion

--- 08.DOM Introduction - Exercise/03. Accordion/accordion.js
+++ 08.DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,24 @@
 function toggle() {
-    console.log('TODO:...');
+//   let buttonRef = document.querySelector("#accordion .head .button");
+
+//   if (buttonRef.textContent === "More") {
+//     buttonRef.textContent = "Less";
+
+//     document.getElementById("extra").style.display = "block";
+//   } else if (buttonRef.textContent === "Less") {
+//     buttonRef.textContent = "More";
+
+//     document.getElementById("extra").style.display = "none";
+//   }
+
+let testEl = document.getElementById("accordion")
+
+console.log(testEl);
+// console.log(testEl.textContent);
+
+
+
+
+
 }
+
