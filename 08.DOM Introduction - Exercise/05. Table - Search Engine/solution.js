eac034c22da0295bce8870d703b6b0db14695b0b
HubanHubanov <huban.hubanov@gmail.com>
2024-01-31 13:26:27 +0200

    add 05.TableSearchEngine

--- 08.DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ 08.DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,26 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  
+  
+  function onClick() {
 
-   function onClick() {
-      //   TODO:
-
-   }
+    const searchInputRef = document.getElementById("searchField");
+    
+    const tableRows = Array.from(document.querySelectorAll("tbody tr"));
+    let searchText = searchInputRef.value;
+    
+    for (let tablerow of tableRows) {
+      let tableData = Array.from(tablerow.querySelectorAll("td"));
+      for (let data of tableData) {
+        
+        if (data.textContent.includes(searchText)) {
+          tablerow.classList.add("select");
+          break;
+        } else {
+          tablerow.classList.remove("select");
+        }
+      }
+    }
+    searchInputRef.value = "";
+  }
 }
