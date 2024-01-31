ba6dcefa790e1ad6952f828d568e94c0436ba1ea
HubanHubanov <huban.hubanov@gmail.com>
2024-02-01 00:53:51 +0200

    add 04.SearchInList

--- 08.DOM Introduction - Exercise/04. Search in List/search.js
+++ 08.DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,22 @@
 function search() {
-   // TODO
+  let searchRef = document.querySelector("#searchText");
+
+  let towns = document.querySelectorAll("#towns li");
+  let matches;
+
+  if (document.querySelector("#result").textContent === "") {
+    matches = 0;
+  } else {
+    matches = Number(document.querySelector("#result").textContent[0]);
+  }
+
+  for (let town of towns) {
+    if (town.textContent.includes(searchRef.value)) {
+      matches++;
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+    } 
+  }
+
+  document.querySelector("#result").textContent = `${matches} matches found`;
 }
