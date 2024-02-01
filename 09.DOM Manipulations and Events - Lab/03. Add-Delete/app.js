c6d5d08523649193bf9e1f61a05aed1b38ce3d59
HubanHubanov <huban.hubanov@gmail.com>
2024-02-02 00:17:03 +0200

    add 03.AddDelete

--- 09.DOM Manipulations and Events - Lab/03. Add-Delete/app.js
+++ 09.DOM Manipulations and Events - Lab/03. Add-Delete/app.js
@@ -1,3 +1,28 @@
 function addItem() {
-    //TODO...
+  const input = document.getElementById("newItemText");
+  const inputText = input.value;
+
+  if (inputText.length == 0) {
+    return;
+  }
+
+  const newLi = document.createElement("li");
+  newLi.textContent = inputText;
+
+  const deleteButton = document.createElement("a");
+  deleteButton.textContent = "[Delete]";
+  deleteButton.href = "#";
+  newLi.appendChild(deleteButton);
+  deleteButton.addEventListener("click", deleteFunction);
+
+  const items = document.getElementById("items");
+  items.appendChild(newLi);
+
+  input.value = "";
+
+  function deleteFunction(event) {
+    const btn = event.target;
+    const liElToDel = btn.parentElement;
+    liElToDel.remove();
+  }
 }
