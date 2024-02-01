622bfa750a54959afb6f4b295f3dacceff926df6
HubanHubanov <huban.hubanov@gmail.com>
2024-02-01 20:13:52 +0200

    add 02.DeleteFromTabble

--- 09.DOM Manipulations and Events - Lab/02. Delete-From-Table/app.js
+++ 09.DOM Manipulations and Events - Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  let input = document.querySelector('[name="email"]');
+  let list = Array.from(document.querySelectorAll("#customers tbody tr"));
+  let result = "Not found.";
+
+  for (let el of list) {
+    if (el.children[1].textContent === input.value) {
+      el.remove();
+
+      result = "Deleted.";
+    }
+  }
+
+  const output = document.getElementById("result");
+  output.textContent = result;
 }
+ 
