1186792e3def2fb2c3f5bc0ae8c980d22c0c51de
HubanHubanov <huban.hubanov@gmail.com>
2024-02-01 13:47:31 +0200

    add 01.ListOfItems

--- 09.DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
+++ 09.DOM Manipulations and Events - Lab/01. List-Of-Items/app.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    
+  let newLi = document.createElement("li");
+  
+  let items = document.getElementById("items");
+  
+  items.appendChild(newLi);
+
+  let textToAdd = document.getElementById("newItemText").value;
+
+  newLi.textContent = textToAdd;
+
 }
