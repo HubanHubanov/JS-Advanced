d7c577a568e9096a33804b9cd99c9e8fa9080fc5
HubanHubanov <huban.hubanov@gmail.com>
2024-02-02 23:25:09 +0200

    add 01.Sections

--- 10.DOM Manipulations and Events - Exercise/01. Sections/script.js
+++ 10.DOM Manipulations and Events - Exercise/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+  const content = document.getElementById("content");
+
+  for (let word of words) {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.textContent = word;
+    div.appendChild(p);
+    p.style.display = "none";
+    content.appendChild(div);
+
+    div.addEventListener("click", myFunc);
+  }
+
+  function myFunc(event) {
+    const currSection = event.target;
+    currSection.children[0].style.display =
+      currSection.children[0].style.display === "none" ? "block" : "none";
+  }
 }
