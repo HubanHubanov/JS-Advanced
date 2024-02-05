fa431c22f9e46d5c5c1f025d33f60ef753d45edc
HubanHubanov <huban.hubanov@gmail.com>
2024-02-05 11:19:04 +0200

    add 03.LockedProfile

--- 10.DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
+++ 10.DOM Manipulations and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+  let buttons = Array.from(document.querySelectorAll("button"));
+
+  buttons.forEach((x) => x.addEventListener("click", onClickHandler));
+
+  function onClickHandler(event) {
+    let hiddenInfo = event.currentTarget.parentElement.querySelector("div");
+
+    let radioBtn = event.currentTarget.parentElement.querySelector(
+      'input[type="radio"]:checked'
+    );
+
+    if (radioBtn.value === "unlock") {
+      if (event.currentTarget.textContent === "Show more") {
+        event.currentTarget.textContent = "Hide it";
+        hiddenInfo.stryle.display = "block";
+      } else {
+        event.currentTarget.textContent = "Show more";
+        hiddenInfo.stryle.display = "none";
+      }
+    }
+  }
 }
