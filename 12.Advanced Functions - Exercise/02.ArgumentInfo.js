9b25cde1651d6a9d161c1e881736f9afbc7f8cd1
HubanHubanov <huban.hubanov@gmail.com>
2024-02-06 22:41:26 +0200

    add 02.ArgumentInfo

---
+++ 12.Advanced Functions - Exercise/02.ArgumentInfo.js
@@ -0,0 +1,30 @@
+function argumentInfo(...arg) {
+  let obj = {};
+
+  for (let el of arg) {
+    let key = typeof el;
+
+    if (!obj.hasOwnProperty(key)) {
+      obj[key] = [el];
+    } else obj[key].push(el);
+
+    console.log(typeof el + ": " + el);
+  }
+
+  let entries = Object.entries(obj);
+
+  let sorted = entries.sort((a, b) => b[1].length - a[1].length);
+
+  for (let [key, value] of entries) {
+    console.log(`${key} = ${value.length}`);
+  }
+}
+
+
+argumentInfo("cat", "dog", 42, 12, 1,  function () {
+  console.log("Hello world!");
+});
+console.log("------------------------");
+argumentInfo("cat", 42, function () {
+  console.log("Hello world!");
+});
