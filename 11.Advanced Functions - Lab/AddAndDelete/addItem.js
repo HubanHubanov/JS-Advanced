f52487fb2cb02a16e4a98051d5f647dd4a432e1c
HubanHubanov <huban.hubanov@gmail.com>
2024-02-06 11:19:05 +0200

    add AddDeleteRows

---
+++ 11.Advanced Functions - Lab/AddAndDelete/addItem.js
@@ -0,0 +1,27 @@
+function addItem() {
+  const inputRef = document.getElementById("newItemText");
+  const inputValue = inputRef.value;
+
+  let newLi = document.createElement("li");
+
+  newLi.textContent = inputValue;
+
+  const itemsRef = document.getElementById("items");
+
+  itemsRef.appendChild(newLi);
+
+  let btn = document.createElement("a");
+  btn.href = "#";
+  btn.textContent = "[Delete]";
+  newLi.appendChild(btn);
+
+  btn.addEventListener("click", eventHandler);
+
+  function eventHandler(event) {
+    let currBtn = event.target;
+    let currRow = currBtn.parentElement;
+    currRow.remove();
+  }
+
+  inputRef.value = "";
+}
