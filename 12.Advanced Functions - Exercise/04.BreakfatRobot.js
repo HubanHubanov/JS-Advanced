3cebbe11e092d5dd9b84675597dc632f1bd8fa03
HubanHubanov <huban.hubanov@gmail.com>
2024-02-07 09:55:03 +0200

    add 04.BreakfastRobot

---
+++ 12.Advanced Functions - Exercise/04.BreakfatRobot.js
@@ -0,0 +1,86 @@
+function solution() {
+  let store = {
+    carbohydrate: 0,
+    protein: 0,
+    fat: 0,
+    flavour: 0,
+  };
+
+  let recipe = {
+    apple: {
+      carbohydrate: 1,
+      flavour: 2,
+    },
+
+    lemonade: {
+      carbohydrate: 10,
+      flavour: 20,
+    },
+
+    burger: {
+      carbohydrate: 5,
+      flavour: 3,
+      fat: 7,
+    },
+
+    eggs: {
+      protein: 5,
+      flavour: 1,
+      fat: 1,
+    },
+
+    turkey: {
+      protein: 10,
+      fat: 10,
+      flavour: 10,
+      carbohydrate: 10,
+    },
+  };
+
+  return function (data) {
+    let [action, type, qty] = data.split(" ");
+
+    switch (action) {
+      case "restock":
+        return restock(type, qty);
+
+        break;
+
+      case "prepare":
+        return prepare(type, qty);
+        break;
+
+      case "report":
+        return report();
+        break;
+    }
+  };
+
+  function restock(type, qty) {
+    store[type] += Number(qty);
+    return "Success";
+  }
+
+  function prepare(type, qty) {
+    let prepareRecipe = {};
+    for (let [el, value] of Object.entries(recipe[type])) {
+      let needElementQty = value * qty;
+      if (store[el] < needElementQty) {
+        return `Error: not enough ${el} in stock`;
+      }
+      prepareRecipe[el] = needElementQty;
+    }
+    for (let [el, value] of Object.entries(prepareRecipe)) {
+      store[el] -= value;
+    }
+    return "Success";
+  }
+
+  function report() {
+    return `protein=${store.protein} carbohydrate=${store.carbohydrate} fat=${store.fat} flavour=${store.flavour}`;
+  }
+}
+
+let manager = solution();
+console.log(manager("restock flavour 50")); // Success
+// console.log(manager("prepare lemonade 4")); // Error: not enough carbohydrate in stock
