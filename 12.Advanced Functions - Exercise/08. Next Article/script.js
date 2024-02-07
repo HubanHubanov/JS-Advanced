47af5bbe1f9c7bc0bafd08a7d8536fa44ae62e52
HubanHubanov <huban.hubanov@gmail.com>
2024-02-07 11:39:33 +0200

    add 08.NextArticle

--- 12.Advanced Functions - Exercise/08. Next Article/script.js
+++ 12.Advanced Functions - Exercise/08. Next Article/script.js
@@ -1,3 +1,16 @@
 function getArticleGenerator(articles) {
-    // TODO
+  let articlesContent = Array.from(articles);
+
+  let content = document.getElementById("content");
+
+  return () => {
+    if (!articlesContent.length) {
+      return;
+    }
+    firtsEl = articlesContent.shift();
+
+    let p = document.createElement("article");
+    p.textContent = firtsEl;
+    content.appendChild(p);
+  };
 }
