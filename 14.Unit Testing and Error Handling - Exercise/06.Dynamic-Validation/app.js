9c099042a2055c2b41229b795fc1f73faad97d9a
HubanHubanov <huban.hubanov@gmail.com>
2024-02-10 01:20:20 +0200

    add 06.DynamicValidation

--- 14.Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
+++ 14.Unit Testing and Error Handling - Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    // TODO
+  let inputRef = document.getElementById("email");
+
+  inputRef.addEventListener("change", eventHandler);
+
+  function eventHandler(e) {
+    let email = e.target.value;
+    let pattern = /^[a-z]+[@]{1}[a-z]+[.]{1}[a-z]+$/gm;
+
+    let match = pattern.exec(email);
+
+    if (!match) {
+      e.target.classList.add("error");
+    } else {
+      e.target.classList.remove("error");
+    }
+  }
 }
