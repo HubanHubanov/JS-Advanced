5675134ee886c604dc948d8863c047f0fc1f2aed
HubanHubanov <huban.hubanov@gmail.com>
2024-02-16 15:45:10 +0200

    update

---
+++ UNIT TESTING/test.js
@@ -0,0 +1,10 @@
+import { Repository } from "./solution.js";
+import { expect } from "chai";
+
+
+
+describe("test", function() {
+  it("works", function() {
+    expect( () => repository.add(entity)).to.throw(1)
+  })
+})
